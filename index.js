f4b26dcddedbe7bfd8839d2f92d8c2c1f4d17ad0
Dara Dermody <dara.dermody@siren.io>
2020-01-16 12:50:51 +0000

    Added comments to clarify some required dependencies

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 module.exports = {
+  // When using this config with Gulp, you must either use gulp version 5 or add the kibana config as a dependency:
+  //   "@elastic/eslint-config-kibana": "^0.7.0"
   extends: [
     '@elastic/eslint-config-kibana'
   ],
+  // You must add this plugin as a dev dependency:
+  //   "eslint-plugin-siren": "sirensolutions/eslint-plugin-siren"
   plugins: [
     'siren'
   ],
