460f6fc7746b5af2ac93f8ad62a86191be82162a
Dara Dermody <daradermody@gmail.com>
2020-01-16 12:52:18 +0000

    Add siren eslint plugin as a peer dependency and fixed the syntax error in the rules (#9)

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 module.exports = {
+  // When using this config with Gulp, you must either use gulp version 5 or add the kibana config as a dependency:
+  //   "@elastic/eslint-config-kibana": "^0.7.0"
   extends: [
     '@elastic/eslint-config-kibana'
   ],
+  // You must add this plugin as a dev dependency:
+  //   "eslint-plugin-siren": "sirensolutions/eslint-plugin-siren"
   plugins: [
     'siren'
   ],
   rules: {
     'siren/memoryleaks': 1
-    "no-unused-vars": 0,
-    "no-var": 1,
-    "prefer-const": 1,
-    "no-extra-semi": 0,
-    "quotes": 0,
-    "import/no-unresolved": 0,
-    "import/named": 0,
-    "import/no-named-as-default-member": 0,
-    "import/no-duplicates": 0,
-    "import/default": 0,
-    "memoryleaks": 1,
-    "keyword-spacing": 2,
-    "comma-spacing": 2
   }
 };
