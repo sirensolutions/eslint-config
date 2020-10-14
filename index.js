8c11bf08cae7fe1a936d23fc0bdfa3f6033f7a64
Dara Dermody <dara.dermody@siren.io>
2020-10-14 16:24:38 +0100

    Enable rule for syncing dependency versions with core

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = {
   ],
   rules: {
     'siren/memory-leak': "warn",
+    'siren/same-core-dependency-version': 'warn',
     "no-console": ["warn", { allow: ["warn", "error"] }]
   }
 };
